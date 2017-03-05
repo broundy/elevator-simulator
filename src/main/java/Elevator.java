93a340de67efee8a112036e58e546acb63a75d5b
Brian Roundy <brian.roundy@gmail.com>
2017-03-05 11:19:45 -0700

    added requirements 2 and 3

--- src/main/java/Elevator.java
+++ src/main/java/Elevator.java
@@ -1,15 +1,42 @@
 public class Elevator{
     public int floors;
+    public int currentFloor;
+    public boolean doorsOpen;
 
     public Elevator(){
-        floors = 2;
+        this(2);
     }
 
     public Elevator(int f){
         floors = f;
+        currentFloor = 1;
+        doorsOpen = false;
     }
 
     public int getFloors() {
         return floors;
     } 
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    } 
+
+    public boolean getDoorsOpen() {
+        return doorsOpen;
+    } 
+
+    public void moveToFloor(int destination){
+        System.out.println("Moving to floor " + destination);
+        currentFloor = destination;
+    }
+
+    public void openDoors(){
+        doorsOpen = true;
+        System.out.println("Opening doors on floor " + currentFloor);
+    }
+
+    public void closeDoors(){
+        doorsOpen = false;
+        System.out.println("Closing doors on floor " + currentFloor);
+    }
 }
--- src/test/java/ElevatorTest.java
+++ src/test/java/ElevatorTest.java
@@ -3,10 +3,32 @@ import static org.junit.Assert.*;
 
 public class ElevatorTest{
 
+
     @Test
     public void testElevatorHasCorrectNumberOfFloors() {
         int floors = 20;
         Elevator classUnderTest = new Elevator(floors);
         assertEquals("elevator needs to have correct number of floors", floors, classUnderTest.getFloors());
+    }   
+
+    @Test
+    public void testMoveElevatorToFloor() {
+        int destination = 2;
+        int floors = 20;
+        Elevator classUnderTest = new Elevator(floors);
+        assertEquals("current floor should be 1", 1, classUnderTest.getCurrentFloor());
+        classUnderTest.moveToFloor(destination);
+        assertEquals("current floor should match destination", destination, classUnderTest.getCurrentFloor());
+    }   
+
+    @Test
+    public void testOpenAndCloseDoors() {
+        int floors = 20;
+        Elevator classUnderTest = new Elevator(floors);
+        assertEquals("doors should be closed", false, classUnderTest.getDoorsOpen());
+        classUnderTest.openDoors();
+        assertEquals("doors should be open", true, classUnderTest.getDoorsOpen());
+        classUnderTest.closeDoors();
+        assertEquals("doors should be closed", false, classUnderTest.getDoorsOpen());
     }   
 }
